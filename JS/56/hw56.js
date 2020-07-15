65d6839d6dec84e68ff790e86e1dcac5a5bdbbf7
ylapciuc <ylapciuc59@yahoo.com>
2020-07-15 12:13:05 -0400

    HW56

--- JS/56/hw56.js
+++ JS/56/hw56.js
@@ -23,3 +23,37 @@ const week = (function(){
 
 console.log(week.getday(7));
 console.log(week.getIndex('sunday'));
+
+
+const calculatorMod = (function(){
+   
+    'use strict';
+ 
+    return{
+ 
+    setRate: function(rate){
+
+        return rate;
+    },
+
+    setYears: function(years){
+        
+        return years;
+    },
+
+    calculateInterest:function(principle, rate , years){
+      return principle * this.setRate(rate) * this.setYears(years);
+    }
+ };
+  
+})();
+
+console.log(calculatorMod.calculateInterest(100,1.1 , 3));
+
+
+
+
+
+ // console.log('Rate:', calculatorMod.setRate(1.1) ,`  
+//Years: ${calculatorMod.setYears(2)}  
+ //${calculatorMod.calculateInterest()}`);
